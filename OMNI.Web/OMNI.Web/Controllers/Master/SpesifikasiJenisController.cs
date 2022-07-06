447708b6bd67404e30809c68ad2d96c8fec60a82
Fajar Sidik <fajarsidikz43@gmail.com>
2022-07-06 13:21:27 +0700

    update spesifikasi Jenis

---
+++ OMNI.Web/OMNI.Web/Controllers/Master/SpesifikasiJenisController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OMNI.Web.Controllers.Master
+{
+    [AllowAnonymous]
+    public class SpesifikasiJenisController : BaseController
+    {
+        private static readonly string INDEX = "~/Views/Master/SpesifikasiJenis/Index.cshtml";
+        private static readonly string ADD_EDIT = "~/Views/Master/SpesifikasiJenis/AddEdit.cshtml";
+
+        public IActionResult Index()
+        {
+            return View(INDEX);
+        }
+
+        [HttpPost("List")]
+        public IActionResult GetAllAccount()
+        {
+            return Ok();
+        }
+
+        [HttpGet("ManageSpesifikasi")]
+        [HttpGet("ManageSpesifikasi/{id:int}")]
+        public IActionResult AddEdit(int id)
+        {
+            ViewBag.FormTitle = "Add & Edit Spesifikasi / Jenis";
+            return PartialView(ADD_EDIT);
+        }
+
+        [HttpPost("ManageSpesifikasi")]
+        [HttpPost("ManageSpesifikasi/{id:int}")]
+        public IActionResult AddEdit([FromBody] object account, [FromRoute] int id)
+        {
+            return Ok();
+        }
+    }
+}
