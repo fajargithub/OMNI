35837ac99499f7a4e080adecbe91c506afec87d4
Fajar Sidik <fajarsidikz43@gmail.com>
2022-07-06 17:55:48 +0700

    update qr code

--- OMNI.Web/OMNI.Web/Controllers/Master/SpesifikasiJenisController.cs
+++ OMNI.Web/OMNI.Web/Controllers/Master/SpesifikasiJenisController.cs
@@ -9,6 +9,9 @@ namespace OMNI.Web.Controllers.Master
         private static readonly string INDEX = "~/Views/Master/SpesifikasiJenis/Index.cshtml";
         private static readonly string ADD_EDIT = "~/Views/Master/SpesifikasiJenis/AddEdit.cshtml";
 
+        private static readonly string DETAIL_INDEX = "~/Views/Master/SpesifikasiJenis/DetailIndex.cshtml";
+        private static readonly string DETAIL_ADD_EDIT = "~/Views/Master/SpesifikasiJenis/AddEditDetail.cshtml";
+
         public IActionResult Index()
         {
             return View(INDEX);
@@ -31,6 +34,26 @@ namespace OMNI.Web.Controllers.Master
         [HttpPost("ManageSpesifikasi")]
         [HttpPost("ManageSpesifikasi/{id:int}")]
         public IActionResult AddEdit([FromBody] object account, [FromRoute] int id)
+        {
+            return Ok();
+        }
+
+        public IActionResult DetailIndex()
+        {
+            return View(DETAIL_INDEX);
+        }
+
+        [HttpGet("ManageDetailSpesifikasi")]
+        [HttpGet("ManageDetailSpesifikasi/{id:int}")]
+        public IActionResult DetailAddEdit(int id)
+        {
+            ViewBag.FormTitle = "Add & Edit Detail Spesifikasi / Jenis";
+            return PartialView(DETAIL_ADD_EDIT);
+        }
+
+        [HttpPost("ManageDetailSpesifikasi")]
+        [HttpPost("ManageDetailSpesifikasi/{id:int}")]
+        public IActionResult DetailAddEdit([FromBody] object account, [FromRoute] int id)
         {
             return Ok();
         }
