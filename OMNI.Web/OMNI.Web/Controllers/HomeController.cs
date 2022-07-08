8f65e684128cbdc082f7a1af76454a2426057af7
Fajar Sidik <fajarsidikz43@gmail.com>
2022-07-08 10:26:38 +0700

    update OMNI

--- OMNI.Web/OMNI.Web/Controllers/HomeController.cs
+++ OMNI.Web/OMNI.Web/Controllers/HomeController.cs
@@ -14,6 +14,9 @@ namespace OMNI.Web.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private static readonly string ADD_EDIT_LLP = "~/Views/Home/AddEditLLP.cshtml";
+        private static readonly string ADD_EDIT_PERSONIL = "~/Views/Home/AddEditPersonil.cshtml";
+        private static readonly string ADD_EDIT_LATIHAN = "~/Views/Home/AddEditLatihan.cshtml";
 
         public HomeController(ILogger<HomeController> logger)
         {
@@ -29,6 +32,48 @@ namespace OMNI.Web.Controllers
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        [HttpGet("ManageLLP")]
+        [HttpGet("ManageLLP/{id:int}")]
+        public IActionResult AddEditLLP(int id)
+        {
+            return PartialView(ADD_EDIT_LLP);
+        }
+
+        [HttpPost("ManageLLP")]
+        [HttpPost("ManageLLP/{id:int}")]
+        public IActionResult AddEditLLP([FromBody] object account, [FromRoute] int id)
+        {
+            return Ok();
+        }
+
+        [HttpGet("ManageTrxPersonil")]
+        [HttpGet("ManageTrxPersonil/{id:int}")]
+        public IActionResult AddEditTrxPersonil(int id)
+        {
+            return PartialView(ADD_EDIT_PERSONIL);
+        }
+
+        [HttpPost("ManageTrxPersonil")]
+        [HttpPost("ManageTrxPersonil/{id:int}")]
+        public IActionResult AddEditTrxPersonil([FromBody] object account, [FromRoute] int id)
+        {
+            return Ok();
+        }
+
+        [HttpGet("ManageTrxLatihan")]
+        [HttpGet("ManageTrxLatihan/{id:int}")]
+        public IActionResult AddEditTrxLatihan(int id)
+        {
+            return PartialView(ADD_EDIT_LATIHAN);
+        }
+
+        [HttpPost("ManageTrxLatihan")]
+        [HttpPost("ManageTrxLatihan/{id:int}")]
+        public IActionResult AddEditTrxLatihan([FromBody] object account, [FromRoute] int id)
+        {
+            return Ok();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
