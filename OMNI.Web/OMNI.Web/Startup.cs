ea6100b44592c466ccbaa3effccd8f289537c403
Fajar Sidik <fajarsidikz43@gmail.com>
2022-07-13 10:46:16 +0700

    update

--- OMNI.Web/OMNI.Web/Startup.cs
+++ OMNI.Web/OMNI.Web/Startup.cs
@@ -1,12 +1,11 @@
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using OMNI.Web.Extensions;
 using System;
 using System.Collections.Generic;
@@ -24,33 +23,11 @@ namespace OMNI.Web
 
         public IConfiguration Configuration { get; }
 
-        //This method gets called by the runtime.Use this method to add services to the container.
+        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.ConfigureDatabaseConnection(Configuration);
-
-            services.ConfigureIdentity(Configuration);
-
-            services.ConfigureSession();
-
-            services.ConfigureDataLayer();
-
-            services.ConfigureDomainLayer();
-
-            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
-
-            services.AddAntiforgery(o => o.HeaderName = "XSRF-TOKEN");
-
-            services.ConfigureHTTPClientFactory(Configuration);
-
-            services.AddResponseCaching();
-
-            services.AddControllersWithViews(opt =>
-            {
-                AuthorizationPolicy policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
-                opt.Filters.Add(new AuthorizeFilter(policy));
-                opt.Filters.Add<ViewBagFilter>();
-            }).AddRazorRuntimeCompilation();
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -60,30 +37,16 @@ namespace OMNI.Web
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+
             app.UseHttpsRedirection();
-            app.UseStaticFiles();
 
             app.UseRouting();
 
-            app.UseAuthentication();
-
             app.UseAuthorization();
-
-            //app.UseHeaderPropagation();
-
-            app.UseSession();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapControllers();
             });
         }
     }
