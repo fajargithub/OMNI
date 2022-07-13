abf3af9752e6fb2aa2c70c1aed9bf84686614344
Fajar Sidik <fajarsidikz43@gmail.com>
2022-07-13 09:50:20 +0700

    update peralatanOSR

---
+++ OMNI.Web/OMNI.Web/Controllers/Base/PeralatanOSRBaseController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OMNI.Web.Controllers.Base
+{
+    public class PeralatanOSRBaseController : BaseController
+    {
+        //protected 
+        public PeralatanOSRBaseController() : base()
+        {
+
+        }
+    }
+}
