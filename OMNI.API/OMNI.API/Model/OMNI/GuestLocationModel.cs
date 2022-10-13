0a8c58f1e7d53f483ac5f57324ee55d51fe2ffa9
Fajar Sidik <fajarsidikz43@gmail.com>
2022-10-13 11:25:38 +0700

    update

---
+++ OMNI.API/OMNI.API/Model/OMNI/GuestLocationModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OMNI.API.Model.OMNI
+{
+    public class GuestLocationModel : BaseModel
+    {
+        public int UserId { get; set; }
+        public string GuestCategory { get; set; }
+        public string Port { get; set; }
+    }
+}
