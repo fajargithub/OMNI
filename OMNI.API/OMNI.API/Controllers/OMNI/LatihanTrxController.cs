399d2d36bc9376d63e2d239a8d980c615c4fd637
Fajar Sidik <fajarsidikz43@gmail.com>
2022-10-17 03:17:22 +0700

    update

--- OMNI.API/OMNI.API/Controllers/OMNI/LatihanTrxController.cs
+++ OMNI.API/OMNI.API/Controllers/OMNI/LatihanTrxController.cs
@@ -15,7 +15,7 @@ using OMNI.Utilities.Constants;
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
+using System.Linq; 
 using System.Threading;
 using System.Threading.Tasks;
 
